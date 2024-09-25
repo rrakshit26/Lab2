500de97ba7941d3e15c49af85557e39ccf7ccbe1
Rakshit <rrakshit@myseneca.ca>
2024-09-25 13:27:20 -0400

    lab2e added

---
+++ lab2e.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# Author: Your Name
+# Author ID: your_seneca_id
+# Date Created: yyyy/mm/dd
+
+timer = 10
+while timer > 0:
+    print(timer)
+    timer -= 1
+print("blast off!")
