7fe1a963e439d3152e7d36e3439476c83fd30712
Rakshit <rrakshit@myseneca.ca>
2024-09-25 13:28:26 -0400

    lab2f added

---
+++ lab2f.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# Author: Your Name
+# Author ID: your_seneca_id
+# Date Created: yyyy/mm/dd
+import sys
+
+timer = int(sys.argv[1])
+
+while timer > 0:
+    print(timer)
+    timer -= 1
+print("blast off!")
